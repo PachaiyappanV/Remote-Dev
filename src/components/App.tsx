e4588ab4255053942af9ed83cff31cb2ee914623
PachaiyappanV <pachaiyappan394@gmail.com>
2024-07-15 10:46:13 +0530

    feat: add custom hook for data fetching

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -13,25 +13,11 @@ import JobList from "./JobList";
 import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import SidebarTop from "./SidebarTop";
+import { useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const [jobItems, setJobItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    if (!searchText) return;
-    const fetchData = async () => {
-      setIsLoading(true);
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
-      const data = await response.json();
-      setIsLoading(false);
-      setJobItems(data.jobItems);
-    };
-    fetchData();
-  }, [searchText]);
-
+  const { isLoading, jobItems } = useJobItems(searchText);
   return (
     <>
       <Background />
