7ae8bbf77623996f3fb01752e73e684134ff7316
PachaiyappanV <pachaiyappan394@gmail.com>
2024-07-19 17:33:28 +0530

    feat: add pagination to job listing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,14 +15,25 @@ import SidebarTop from "./SidebarTop";
 import { useDebounce, useJobItems } from "../lib/hooks";
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
+import PaginationControls from "./PaginationControls";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const debouncedValue = useDebounce(searchText, 300);
   const { jobItems, isLoading } = useJobItems(debouncedValue);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const totalNumberOfResults = jobItems.length;
+  const totalNumberOfPages = totalNumberOfResults / 7;
+  const jobItemsSliced = jobItems.slice(currentPage * 7 - 7, currentPage * 7);
 
+  const handlePageChange = (direction: "prev" | "next") => {
+    if (direction === "prev") {
+      setCurrentPage((prev) => prev - 1);
+    } else if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
   return (
     <>
       <Background />
@@ -41,7 +52,12 @@ function App() {
             <ResultsCount totalNumberOfResults={totalNumberOfResults} />
             <SortingControls />
           </SidebarTop>
-          <JobList jobItems={jobItems} isLoading={isLoading} />
+          <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
+          <PaginationControls
+            currentPage={currentPage}
+            onClick={handlePageChange}
+            totalNumberOfPages={totalNumberOfPages}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,35 @@
-export default function Pagination() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+export default function PaginationControls({
+  currentPage,
+  onClick,
+  totalNumberOfPages,
+}: {
+  currentPage: number;
+  onClick: (direction: "prev" | "next") => void;
+  totalNumberOfPages: number;
+}) {
+  return (
+    <section className="pagination">
+      {currentPage > 1 && (
+        <button
+          className="pagination__button pagination__button--back"
+          onClick={() => onClick("prev")}
+        >
+          <ArrowLeftIcon />
+          Page {currentPage - 1}
+        </button>
+      )}
+
+      {totalNumberOfPages > currentPage && (
+        <button
+          className="pagination__button pagination__button--next"
+          onClick={() => onClick("next")}
+        >
+          <ArrowRightIcon />
+          Page {currentPage + 1}
+        </button>
+      )}
+    </section>
+  );
 }
