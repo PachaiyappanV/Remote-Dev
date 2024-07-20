3b088223e1824fe3ad5f27bf52c49a93fe5a2282
PachaiyappanV <pachaiyappan394@gmail.com>
2024-07-20 15:14:48 +0530

    feat: implement storing bookmarked job items

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,9 +1,22 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { useContext } from "react";
+import { BookmarksContext } from "../contexts/BookmarksContextProvider";
 
-export default function BookmarkIcon() {
+export default function BookmarkIcon({ id }: { id: number }) {
+  const { bookmarkedIds, handleToggleBookmark } = useContext(BookmarksContext);
+  console.log(bookmarkedIds);
   return (
-    <button className="bookmark-btn">
-      <BookmarkFilledIcon className="filled" />
+    <button
+      className="bookmark-btn"
+      onClick={(e) => {
+        handleToggleBookmark(id);
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <BookmarkFilledIcon
+        className={`${bookmarkedIds.includes(id) ? "filled" : ""}`}
+      />
     </button>
   );
 }
