adf67470ab11f4d5856fe1e7a2af14385a1c9d0b
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-16 18:04:30 +0530

    fix: reove as oconst in useLocalStorage hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -128,14 +128,14 @@ export const useLocalStorage = <T>(
   key: string,
   initialValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] => {
-  const [value, setValue] = useState<T>(() =>
+  const [value, setValue] = useState(() =>
     JSON.parse(localStorage.getItem(key) || JSON.stringify(initialValue))
   );
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
-  return [value, setValue] as const;
+  return [value, setValue];
 };
 
 export const useBookmarksContext = () => {
